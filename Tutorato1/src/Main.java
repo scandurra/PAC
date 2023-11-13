d03bfed814a976bf3d1dcc8b924306fba803a9d6
Michele Beretta <beretta.m98@gmail.com>
2023-11-13 11:39:08 +0100

    Add Javadoc

--- Tutorato1/src/Main.java
+++ Tutorato1/src/Main.java
@@ -1,8 +1,29 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
 public class Main {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+	}
+	
+	/**
+	 * Sum.
+	 *
+	 * @param a First number
+	 * @param b Second number
+	 * @return Sum of the two
+	 */
+	@SuppressWarnings("unused")
+	private static int sum(int a, int b) {
+		return a + b;
 	}
 
 }
