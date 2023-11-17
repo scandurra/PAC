a1a9adc5a6f9342911c85e69d799142fa76ff6a8
Michele Beretta <beretta.m98@gmail.com>
2023-11-17 11:27:55 +0100

    Tutorato 3 - Codice completo

---
+++ Tutorato3_Calcolatrice/CalcolatriceTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcolatriceTest {
+
+	@Test
+	void test() {
+		assertEquals(8, Calcolatrice.pow(2, 3));
+	}
+
+}
